99ccd2d8a31ee033681966cb06a0b302df18931f
susantisanti <itnas.santie@gmail.com>
2021-03-10 06:52:58 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote 2</title>
+</head>
+<body>
+    <h1>Hello Duniaku</h1>
+<p>Ilmu itu tidak ada habisnya, semakin berkembangnya dunia tekhnologi menuntut manusia untuk semakin membuka wawasannya.<br>
+Di era revolusi industri 4.0 seperti sekarang ini, dunia digital sangat berperan dalam memudahkan pekerjaan manusia,<br>
+</p>
+</body>
+</html>
